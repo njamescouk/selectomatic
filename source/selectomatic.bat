b9640e495f2ab300e17782b080ccc8d9be659e3c
nick james <null@example.com>
2018-01-24 12:44:22 +0000

    parameterised source db
    started folding into selectomatic.sql

--- source/selectomatic.bat
+++ source/selectomatic.bat
@@ -1,14 +1,38 @@
-@echo off
+rem @echo off
+if "%1"=="" echo no db given & goto usage
+if "%1"=="-h" goto usage
+
+set SELECTO_DB=selectomatic.db
+set SELECTO_INPUT=%1
+
 if exist selectoNames.txt del selectoNames.txt
 if exist selectoRows.txt del selectoRows.txt
-sqlite3 ..\example\example.db "SELECT "name" FROM "sqlite_master"  WHERE type='table' OR type='view';" > selectoNames.txt
-for /f %%t IN (selectoNames.txt) do sqlite3 -separator , -header ..\example\example.db "SELECT '%%t' AS %%t, * FROM %%t LIMIT 1;" | sed -n 1p;q >> selectoRows.txt
 
+rem get table names and field names
+sqlite3 %SELECTO_INPUT% "SELECT "name" FROM "sqlite_master"  WHERE type='table' OR type='view';" > selectoNames.txt
+for /f %%t IN (selectoNames.txt) do sqlite3 -separator , -header %SELECTO_INPUT% ^
+        "SELECT '%%t' AS %%t, * FROM %%t LIMIT 1;" | sed -n 1p;q >> selectoRows.txt
+
+rem rotate so we've got a table
 echo "tableOrView","field"> tablesAndFields.csv
 csvrewrite -pivot selectoRows.txt>> tablesAndFields.csv
 
-if exist selectomatic.db del selectomatic.db
-sqliteimport selectomatic.db tablesAndFields.csv
-sqlite3 selectomatic.db < views.sql
+rem import into selecto db and add views for selecto html
+if exist %SELECTO_DB% del %SELECTO_DB%
+sqliteimport %SELECTO_DB% tablesAndFields.csv
+sqlite3 %SELECTO_DB% < views.sql
 
-sqlite3 selectomatic.db "SELECT elements FROM page;" | htmlwrap > selectomatic.html
+rem bingo
+sqlite3 %SELECTO_DB% "SELECT elements FROM page;" | htmlwrap > selectomatic.html
+
+set SELECTO_DB=
+set SELECTO_INPUT=
+
+goto :eof
+
+:usage
+
+echo %0 dbFile
+echo produces selectomatic.html which enables interactive choice
+echo of fields to display from  tables in dbfile.
+goto :eof
--- source/selectomatic.sql
+++ source/selectomatic.sql
@@ -1,3 +1,59 @@
 -- selectomatic.sql
-SELECT "name", "sql"  FROM "sqlite_master"  WHERE type='table' OR type='view';
+-- has to be invoked with sqlite3
 
+CREATE TEMP TABLE tvNames
+AS SELECT "name" FROM "sqlite_master"  WHERE type='table' OR type='view';
+
+CREATE TEMP TABLE pragmaStmts
+AS SELECT 'PRAGMA table_info(' || name || ');' AS stmt FROM temp.tvNames;
+
+.output selectoPragma.sql
+SELECT stmt FROM temp.pragmaStmts;
+.output
+
+.output selectoColumnInfo.csv
+--.header on
+.mode csv
+.read selectoPragma.sql
+.mode list
+.header off
+.output
+
+/*
+"SELECT '%%t' AS %%t, * FROM %%t LIMIT 1;"
+
+--create views
+CREATE VIEW "inputs" AS 
+SELECT tableOrView AS parent
+, field
+,'<input type="checkbox" onclick="doFieldChange(this)" name="' || tableOrView || '" value="' || field || '"></input>' AS element
+FROM "tablesAndFields";
+
+
+CREATE VIEW "labels" AS 
+SELECT parent
+,'<label>
+' || element || '
+' || field || '
+</label>
+' AS element
+FROM "inputs";
+
+CREATE VIEW "fieldsets" AS 
+SELECT '<fieldset>
+<legend>' || parent || '</legend>
+' || group_concat(element, '
+') || '
+</fieldset>' AS element
+FROM "labels"
+GROUP BY parent;
+
+CREATE VIEW "page" AS 
+SELECT '<h2>Selectomatic!</h2>
+<h3>your user friendly interactive field selector</code></h3>
+' || group_concat(element, '
+') || '
+<textarea id="sqlText" placeholder="sql will appear here, as if by magic"></textarea>
+' AS elements
+FROM fieldsets;
+*/
