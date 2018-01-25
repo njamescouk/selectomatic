3c89512fbe8e081e39a5e589a9c6bae572de7cf5
nick james <null@example.com>
2018-01-25 23:53:33 +0000

    just about working if incomplete

---
+++ source/tcl/selectomatic.tcl
@@ -0,0 +1,34 @@
+package require Tk
+package require sqlite3
+
+wm iconbitmap . selectoIcon.ico
+wm title . Selectomatic
+
+label .heading -font {normal -32} -justify center -text "Selectomatic\nDatabase Viewer"
+pack .heading
+
+set inputGroup [labelframe .fileSelectorGroup -text "Select database file to open:"]
+
+entry $inputGroup.ent -width 40
+button $inputGroup.but -text "Browse ..." -command "fileDialog $inputGroup $inputGroup.ent"
+pack $inputGroup.ent -side left -padx 10 -expand yes -fill x
+pack $inputGroup.but -side left -padx 10 -pady 3
+pack $inputGroup -fill x -padx 2c -pady 3
+
+proc fileDialog {w ent} {
+    #   Type names      Extension(s)    Mac File Type(s)
+    #
+    #---------------------------------------------------------
+    set types {
+        {"sqlite db files" {.db .sqlite .etilqs}}
+        {"All files"       *}
+    }
+
+    set uf [tk_getOpenFile -multiple false -filetypes $types -parent $w -typevariable "sqlite db files"]
+
+    if {[string compare $uf ""]} {
+        $ent delete 0 end
+        $ent insert 0 $uf
+        $ent xview end
+    }
+}
